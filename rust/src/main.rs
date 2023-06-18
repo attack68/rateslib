f3ffdae5dd3a63f1c7390536a781ac415e480397
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2023-06-18 23:08:40 +0200

    ENH: begin to transfer some methods to Rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,14 @@
-extern crate ndarray;
+// extern crate ndarray;
 
 use std::ops;
-use ndarray::{Array1, Array, arr1, arr2, array};
+use ndarray::{Array1, Array, arr1};
 // use ndarray_einsum_beta::*;
 use num_traits;
+use num_traits::Pow;
+
 // use indexmap::indexset;
 use indexmap::set::IndexSet;
+use auto_ops::{impl_op};
 
 fn is_close(a: &f64, b: &f64, abs_tol: Option<f64>) -> bool {
     // used rather than equality for float numbers
@@ -36,19 +39,20 @@ impl Dual {
         }
     }
 
-    // pub fn pow(&self, arg: f64) -> Dual {
+    // pub fn pow(&self, power: f64) -> Dual {
     //     return Dual {
-    //         real: self.real.pow(arg),
+    //         real: self.real.pow(power),
     //         vars: self.vars,
-    //         dual: Array1::from_iter()
+    //         dual: self.dual * power * self.real.pow(power - 1.0),
     //     }
     // }
 
-    fn upcast_combined(self, other: Dual) -> (Dual, Dual) {
+    fn to_combined_vars(self, other: Dual) -> (Dual, Dual) {
         // check the set of vars in each Dual are equivalent
-        if self.vars_check(&other) {
+        if self.is_same_vars(&other) {
             // if the same vars then reproduce lhs and upcast rhs (in case of order differences)
-            (self.clone(), other.to_new_vars(&self.vars))
+            let recast = other.to_new_vars(&self.vars);
+            (self, recast)
         } else {
             // upcast both Duals to have same ordered vars
             let comb_vars = IndexSet::from_iter(self.vars.union(&other.vars).map(|x| x.clone()));
@@ -79,91 +83,9 @@ impl Dual {
         }
     }
 
-    fn vars_check(&self, other: &Dual) -> bool {
+    fn is_same_vars(&self, other: &Dual) -> bool {
         // test if the vars of a Dual have the same elements but possibly a different order
         return self.vars.len() == other.vars.len() && self.vars.intersection(&other.vars).count() == self.vars.len()
-    }
-}
-
-impl ops::Add<f64> for Dual {
-    type Output = Dual;
-    fn add(self, other: f64) -> Dual {
-        Dual {vars: self.vars, real: self.real + other, dual: self.dual}
-    }
-}
-
-impl ops::Sub<f64> for Dual {
-    type Output = Dual;
-    fn sub(self, other: f64) -> Dual {
-        Dual {vars: self.vars, real: self.real - other, dual: self.dual}
-    }
-}
-
-impl ops::Mul<f64> for Dual {
-    type Output = Dual;
-    fn mul(self, other: f64) -> Dual {
-        Dual {vars: self.vars, real: self.real * other, dual: self.dual * other}
-    }
-}
-
-impl PartialEq<f64> for Dual {
-    fn eq(&self, other: &f64) -> bool {
-        let d = Dual::new(*other, &[], &[]);
-        return d == *self;
-    }
-}
-
-impl ops::Neg for Dual {
-    type Output = Dual;
-    fn neg(self) -> Dual {
-        Dual {vars: self.vars, real: -self.real, dual: -self.dual}
-    }
-}
-
-impl std::iter::Sum for Dual {
-    fn sum<I>(iter: I) -> Self
-       where I: Iterator<Item = Dual>
-    {
-        return iter.fold(Dual::new(0.0, &[], &[]), |acc, x| acc + x)
-        // let mut d = Dual::new(0.0, &[], &[]);
-        // for val in iter { d = d + val }
-        // return d
-    }
-}
-
-impl ops::Add<Dual> for Dual {
-    type Output = Dual;
-    fn add(self, other: Dual) -> Dual {
-        let (x, y) = self.upcast_combined(other);
-        Dual {
-            real: x.real + y.real,
-            dual: x.dual + y.dual,
-            vars: x.vars,
-        }
-    }
-}
-
-impl ops::Sub<Dual> for Dual {
-    type Output = Dual;
-    fn sub(self, other: Dual) -> Dual {
-        let (x, y) = self.upcast_combined(other);
-        Dual {
-            real: x.real - y.real,
-            dual: x.dual - y.dual,
-            vars: x.vars
-        }
-    }
-}
-
-impl ops::Mul<Dual> for Dual {
-    type Output = Dual;
-    fn mul(self, other: Dual) -> Dual {
-        let (x, y) = self.upcast_combined(other);
-        Dual {
-            real: x.real * y.real,
-            dual: x.dual * y.real + y.dual * x.real,
-            vars: x.vars
-        }
     }
 }
 
@@ -183,12 +105,131 @@ impl num_traits::identities::Zero for Dual {
     }
 }
 
+impl Pow<f64> for Dual {
+    type Output = Dual;
+    fn pow(self, power: f64) -> Dual {
+        return Dual {
+            real: self.real.pow(power),
+            vars: self.vars,
+            dual: self.dual * power * self.real.pow(power - 1.0),
+        }
+    }
+}
+
+
+// impl_op_ex_commutative!(+ |a: &Dual, b: &f64| -> Dual { Dual {vars: a.vars, real: a.real + b, dual: a.dual} });
+
+impl_op!(- |a: Dual| -> Dual { Dual {vars: a.vars, real: -a.real, dual: -a.dual}});
+
+impl_op!(+ |a: Dual, b: f64| -> Dual { Dual {vars: a.vars, real: a.real + b, dual: a.dual} });
+impl_op!(+ |a: f64, b: Dual| -> Dual { Dual {vars: b.vars, real: b.real + a, dual: b.dual} });
+impl_op!(+ |a: Dual, b: Dual| -> Dual {
+    let (x, y) = a.to_combined_vars(b);
+    Dual {real: x.real + y.real, dual: x.dual + y.dual, vars: x.vars}
+});
+
+impl_op!(- |a: Dual, b: f64| -> Dual { Dual {vars: a.vars, real: a.real - b, dual: a.dual} });
+impl_op!(- |a: f64, b: Dual| -> Dual { Dual {vars: b.vars, real: a - b.real, dual: -b.dual} });
+impl_op!(- |a: Dual, b: Dual| -> Dual {
+    let (x, y) = a.to_combined_vars(b);
+    Dual {real: x.real - y.real, dual: x.dual - y.dual, vars: x.vars}
+});
+
+impl_op!(* |a: Dual, b: f64| -> Dual { Dual {vars: a.vars, real: a.real * b, dual: a.dual * b} });
+impl_op!(* |a: f64, b: Dual| -> Dual { Dual {vars: b.vars, real: a * b.real, dual: b.dual * a} });
+impl_op!(* |a: Dual, b: Dual| -> Dual {
+    let (x, y) = a.to_combined_vars(b);
+    Dual {real: x.real * y.real, dual: x.dual * y.real + y.dual * x.real, vars: x.vars}
+});
+
+impl_op!(/ |a: Dual, b: f64| -> Dual { Dual {vars: a.vars, real: a.real / b, dual: a.dual / b} });
+impl_op!(/ |a: f64, b: Dual| -> Dual { a * b.pow(-1.0) });
+impl_op!(/ |a: Dual, b: Dual| -> Dual { a * b.pow(-1.0) });
+
+// impl ops::Add<f64> for Dual {
+//     type Output = Dual;
+//     fn add(self, other: f64) -> Dual {
+//         Dual {vars: self.vars, real: self.real + other, dual: self.dual}
+//     }
+// }
+
+// impl ops::Sub<f64> for Dual {
+//     type Output = Dual;
+//     fn sub(self, other: f64) -> Dual {
+//         Dual {vars: self.vars, real: self.real - other, dual: self.dual}
+//     }
+// }
+
+// impl ops::Mul<f64> for Dual {
+//     type Output = Dual;
+//     fn mul(self, other: f64) -> Dual {
+//         Dual {vars: self.vars, real: self.real * other, dual: self.dual * other}
+//     }
+// }
+
+impl PartialEq<f64> for Dual {
+    fn eq(&self, other: &f64) -> bool {
+        return Dual::new(*other, &[], &[]) == *self;
+    }
+}
+
+impl std::iter::Sum for Dual {
+    fn sum<I>(iter: I) -> Self
+    where I: Iterator<Item = Dual> {
+        return iter.fold(Dual::new(0.0, &[], &[]), |acc, x| acc + x)
+    }
+}
+
+// impl ops::Neg for Dual {
+//     type Output = Dual;
+//     fn neg(self) -> Dual {
+//         Dual {vars: self.vars, real: -self.real, dual: -self.dual}
+//     }
+// }
+
+// impl ops::Add<Dual> for Dual {
+//     type Output = Dual;
+//     fn add(self, other: Dual) -> Dual {
+//         let (x, y) = self.to_combined_vars(other);
+//         Dual {
+//             real: x.real + y.real,
+//             dual: x.dual + y.dual,
+//             vars: x.vars,
+//         }
+//     }
+// }
+
+// impl ops::Sub<Dual> for Dual {
+//     type Output = Dual;
+//     fn sub(self, other: Dual) -> Dual {
+//         let (x, y) = self.to_combined_vars(other);
+//         Dual {
+//             real: x.real - y.real,
+//             dual: x.dual - y.dual,
+//             vars: x.vars
+//         }
+//     }
+// }
+
+// impl ops::Mul<Dual> for Dual {
+//     type Output = Dual;
+//     fn mul(self, other: Dual) -> Dual {
+//         let (x, y) = self.to_combined_vars(other);
+//         Dual {
+//             real: x.real * y.real,
+//             dual: x.dual * y.real + y.dual * x.real,
+//             vars: x.vars
+//         }
+//     }
+// }
+
+
 impl PartialEq<Dual> for Dual {
     fn eq(&self, other: &Dual) -> bool {
         if self.real != other.real {
             return false
         }
-        if ! self.vars_check(&other) {
+        if ! self.is_same_vars(&other) {
             return false
         }
         let another = other.to_new_vars(&self.vars);
@@ -222,10 +263,19 @@ fn main() {
     println!("{:?}", arr1_dot(z.clone(), b.clone()))
 }
 
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn add_dual_float {
-//         //
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use crate::Dual;
+
+    #[test]
+    fn add_dual_float() {
+        let result = 10.0 + Dual::new(20.0, &["a"], &[]) + Dual::new(5.0, &["b"], &[2.0]) + 10.0;
+        assert_eq!(result, Dual::new(45.0, &["a", "b"], &[1.0, 2.0]))
+    }
+
+    #[test]
+    fn sub_dual_float() {
+        let result = 100.0 - Dual::new(20.0, &["a"], &[]) - Dual::new(5.0, &["b"], &[2.0]) - 10.0;
+        assert_eq!(result, Dual::new(65.0, &["a", "b"], &[-1.0, -2.0]))
+    }
+}
