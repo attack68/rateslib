b37456293be7ce1c51f007af434fdb7b4de782f9
JHM Darbyshire (MBP) <attack68@users.noreply.github.com>
2023-06-23 10:25:13 +0200

    PERF: add RC to `vars` so that references can be maintained

--- rust/benches/my_benchmark.rs
+++ rust/benches/my_benchmark.rs
@@ -3,6 +3,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rateslib_rust::dual::{Dual, arr1_dot};
 use ndarray::Array;
 use indexmap::set::IndexSet;
+use std::rc::Rc;
 
 fn dual_add_bm(a: &Dual, b: &Dual) -> Dual {
     a + b
@@ -18,8 +19,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     let dual_2 = Array::ones(1000);
     let vars2 = IndexSet::from_iter((0..1000).map(|x| format!("v{}", x).to_string()));
 
-    let a = Dual { real: 2.0, vars: vars, dual: dual_ };
-    let b = Dual { real: 3.0, vars: vars2, dual: dual_2 };
+    let a = Dual { real: 2.0, vars: Rc::new(vars), dual: dual_ };
+    let b = Dual { real: 3.0, vars: Rc::new(vars2), dual: dual_2 };
     let x = 20.1;
     let y = 22.1;
     c.bench_function("float add", |z| z.iter(|| float_add_bm(&x, &y)));
