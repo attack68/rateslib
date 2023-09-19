c2c9ec66617769726f40c5333f58b246027163a3
Xiar-fatah <xiar.fatah@gmail.com>
2023-09-19 19:51:12 +0200

    Update curves.py
    - Added a small example for rates in curves.py.
    - Removed from rateslib.curves import LineCurve from LineCurve example.
    - Added from rateslib import dt at the top of curves.py.

--- rateslib/curves.py
+++ rateslib/curves.py
@@ -3,7 +3,7 @@
    :suppress:
 
    from rateslib.curves import *
-   from datetime import datetime as dt
+   from rateslib import dt
 """
 
 from __future__ import annotations
@@ -443,6 +443,19 @@ class Curve(_Serialize):
           assumed to be comprised of RFR
           rates and an approximation is used to derive to total rate.
 
+        Examples
+        --------
+
+        .. ipython:: python
+
+            curve = Curve(
+                nodes={
+                    dt(2022, 1, 1): 1.0,
+                    dt(2022, 2, 1): 0.98,
+                    dt(2022, 3, 1): 0.978,
+                },
+            )
+            curve.rate(dt(2022, 2, 1), dt(2022, 3, 1))
         """
         modifier = self.modifier if modifier is NoInput.blank else modifier
         # calendar = self.calendar if calendar is False else calendar
@@ -575,9 +588,6 @@ class Curve(_Serialize):
 
         Examples
         --------
-        .. ipython:: python
-
-           from rateslib.curves import Curve
 
         .. ipython:: python
 
@@ -1317,9 +1327,6 @@ class LineCurve(Curve):
 
         Examples
         --------
-        .. ipython:: python
-
-           from rateslib.curves import LineCurve
 
         .. ipython:: python
 
