888fe0f003a53543721c18f83f1c0207e6ed75d2
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2023-10-16 18:04:53 +0200

    ENH: add Curve json serialiser

--- rateslib/json.py
+++ rateslib/json.py
@@ -1,5 +1,7 @@
 from rateslib.default import NoInput
 from rateslib.fx import FXRates
+from rateslib.curves import Curve
+from rateslib.calendars import create_calendar, Holiday
 from typing import Any, Union
 from datetime import datetime
 import json
@@ -7,7 +9,8 @@ import json
 
 class Serialise:
     """
-    Class to create a string based representation of rateslib objects for network transmittance.
+    Class to create a string based representation of rateslib objects for database storage or
+    network transmission.
 
     Parameters
     ----------
@@ -18,15 +21,22 @@ class Serialise:
 
     def __init__(self, obj: Any):
         self.obj = obj
+        self.name = type(obj).__name__
+        self._to_json = getattr(self, f"_{self.name}_to", None)
+        if self._to_json is None:
+            raise NotImplementedError(
+                f"The class {self.name} does not yet have a JSON implementation."
+            )
 
     def to_json(self):
-        type_str = type(self.obj).__name__
-        method = getattr(self, f"_{type_str}_to", None)
-        if method is None:
-            raise NotImplementedError(
-                f"The class {type_str} does not yet have a JSON implementation"
-            )
-        return method()
+        """
+        Convert the *rateslib* object to JSON format for database storage or network transmit.
+
+        Returns
+        -------
+        str
+        """
+        return self._to_json()
 
     @classmethod
     def from_json(cls, obj: Any, data: str):
@@ -35,8 +45,8 @@ class Serialise:
 
         Parameters
         ----------
-        obj : Any
-            The *rateslib* class object to create. E.g. "IRS".
+        obj : Any, str
+            The *rateslib* class object to create. E.g. "IRS" or :class:`~rateslib.instruments.IRS`
         data : str
             The data to constitute the object from in JSON format.
 
@@ -47,7 +57,7 @@ class Serialise:
         Examples
         --------
         """
-        klass = obj.__name__
+        klass = obj if isinstance(obj, str) else obj.__name__
         method = getattr(cls, f"_{klass}_from", None)
         if method is None:
             return NotImplementedError(
@@ -56,20 +66,76 @@ class Serialise:
         return method(data)
 
     def _FXRates_to(self):
-        return json.dumps(
-            {
-                "fx_rates": {k: float(v) for k, v in self.obj.fx_rates.items()},
-                "settlement": _date_or_noinput_to(self.obj.settlement),
-                "base": self.obj.base
-            },
-            default=str
-        )
+        data = {
+            "fx_rates": {k: float(v) for k, v in self.obj.fx_rates.items()},
+            "settlement": _date_or_noinput_to(self.obj.settlement),
+            "base": self.obj.base
+        }
+        return json.dumps(data, default=str)
 
     @classmethod
     def _FXRates_from(cls, data: str):
         kwargs = json.loads(data)
         kwargs["settlement"] = _date_or_noinput_from(kwargs["settlement"])
         return FXRates(**kwargs)
+
+    def _Curve_data(self):
+        return {
+            "nodes": {dt.strftime("%Y-%m-%d"): float(v) for dt, v in self.obj.nodes.items()},
+            "interpolation": self.obj.interpolation if isinstance(self.obj.interpolation, str) else None,
+            "t": _datelist_or_noinput_to(self.obj.t),
+            "c": self.obj.spline.c if self.obj.c_init else None,
+            "id": self.obj.id,
+            "convention": self.obj.convention,
+            "endpoints": self.obj.spline_endpoints,
+            "modifier": self.obj.modifier,
+            "calendar_type": self.obj.calendar_type,
+            "ad": self.obj.ad,
+        }
+
+    def _Curve_to(self):
+        data = self._Curve_data()
+        if data["calendar_type"] == "null":
+            data.update({"calendar": None})
+        elif "named: " in data["calendar_type"]:
+            data.update({"calendar": data.calendar_type[7:]})
+        else:  # calendar type is custom
+            data.update(
+                {
+                    "calendar": {
+                        "weekmask": self.obj.calendar.weekmask,
+                        "holidays": [
+                            d.item().strftime("%Y-%m-%d")
+                            for d in self.obj.calendar.holidays  # numpy/pandas timestamp to py
+                        ],
+                    }
+                }
+            )
+        return json.dumps(data, default=str)
+
+    @classmethod
+    def _Curve_from(cls, data: str):
+        kwargs = json.loads(data)
+        kwargs["nodes"] = {
+            datetime.strptime(dt, "%Y-%m-%d"): v for dt, v in kwargs["nodes"].items()
+        }
+        if kwargs["calendar_type"] == "custom":
+            # must load and construct a custom holiday calendar from serial dates
+            def parse(d: datetime):
+                return Holiday("", year=d.year, month=d.month, day=d.day)
+
+            dates = [
+                parse(datetime.strptime(d, "%Y-%m-%d")) for d in kwargs["calendar"]["holidays"]
+            ]
+            kwargs["calendar"] = create_calendar(
+                rules=dates, weekmask=kwargs["calendar"]["weekmask"]
+            )
+
+        kwargs["t"] = _datelist_or_noinput_from(kwargs["t"])
+        kwargs["c"] = NoInput(0) if kwargs["c"] is None else kwargs["c"]
+
+        serial = {k: v for k, v in kwargs.items() if v is not None}
+        return Curve(**{**serial, **kwargs})
 
 
 def _date_or_noinput_to(date: datetime):
@@ -86,3 +152,17 @@ def _date_or_noinput_from(date: Union[str, None]):
         return NoInput(0)
     else:
         return datetime.strptime(date, "%Y-%m-%d")
+
+
+def _datelist_or_noinput_to(datelist: datetime):
+    if datelist is NoInput.blank:
+        return None
+    else:
+        return [date.strftime('%Y-%m-%d') for date in datelist]
+
+
+def _datelist_or_noinput_from(datelist: Union[str, None]):
+    if datelist is None:
+        return NoInput(0)
+    else:
+        return [datetime.strptime(date, "%Y-%m-%d") for date in datelist]
--- tests/test_json.py
+++ tests/test_json.py
@@ -1,54 +1,167 @@
 import pytest
 from datetime import datetime as dt
-from pandas import DataFrame, Series, date_range, Index
-from pandas.testing import assert_frame_equal, assert_series_equal
-import numpy as np
 
 import context
 from rateslib import default_context
-from rateslib.legs import (
-    FixedLeg,
-    FloatLeg,
-    FloatPeriod,
-    ZeroFloatLeg,
-    ZeroFixedLeg,
-    ZeroIndexLeg,
-    FixedPeriod,
-    CustomLeg,
-    IndexFixedLeg,
-    FloatLegMtm,
-    FixedLegMtm,
-    Cashflow,
-)
 from rateslib.fx import FXRates, FXForwards
 from rateslib.default import Defaults, NoInput
 from rateslib.curves import Curve, IndexCurve
 from rateslib.json import Serialise
 
 
-def test_fxrates_to_json():
-    fxr = FXRates({"usdnok": 8.0, "eurusd": 1.05})
-    result = Serialise(fxr).to_json()
-    expected = '{"fx_rates": {"usdnok": 8.0, "eurusd": 1.05}, "settlement": null, "base": "usd"}'
-    assert result == expected
+class TestFXRates:
 
-    fxr = FXRates({"usdnok": 8.0, "eurusd": 1.05}, dt(2022, 1, 3))
-    result = Serialise(fxr).to_json()
-    expected = (
-        '{"fx_rates": {"usdnok": 8.0, "eurusd": 1.05}, "settlement": "2022-01-03", "base": "usd"}'
-    )
-    assert result == expected
+    def test_to_json(self):
+        fxr = FXRates({"usdnok": 8.0, "eurusd": 1.05})
+        result = Serialise(fxr).to_json()
+        expected = '{"fx_rates": {"usdnok": 8.0, "eurusd": 1.05}, "settlement": null, "base": "usd"}'
+        assert result == expected
+
+        fxr = FXRates({"usdnok": 8.0, "eurusd": 1.05}, dt(2022, 1, 3))
+        result = Serialise(fxr).to_json()
+        expected = (
+            '{"fx_rates": {"usdnok": 8.0, "eurusd": 1.05}, "settlement": "2022-01-03", "base": "usd"}'
+        )
+        assert result == expected
+
+    def test_from_json_and_equality(self):
+        fxr1 = FXRates({"usdnok": 8.0, "eurusd": 1.05})
+        fxr2 = FXRates({"usdnok": 12.0, "eurusd": 1.10})
+        assert fxr1 != fxr2
+
+        fxr2 = Serialise.from_json(FXRates,
+            '{"fx_rates": {"usdnok": 8.0, "eurusd": 1.05}, "settlement": null, "base": "usd"}'
+        )
+        assert fxr2 == fxr1
+
+        fxr3 = FXRates({"usdnok": 8.0, "eurusd": 1.05}, base="NOK")
+        assert fxr1 != fxr3  # base is different
 
 
-def test_fxrates_from_json_and_equality():
-    fxr1 = FXRates({"usdnok": 8.0, "eurusd": 1.05})
-    fxr2 = FXRates({"usdnok": 12.0, "eurusd": 1.10})
-    assert fxr1 != fxr2
+class TestCurve:
 
-    fxr2 = Serialise.from_json(FXRates,
-        '{"fx_rates": {"usdnok": 8.0, "eurusd": 1.05}, "settlement": null, "base": "usd"}'
-    )
-    assert fxr2 == fxr1
+    def test_to_json(self):
+        curve = Curve(
+            nodes={
+                dt(2022, 3, 1): 1.00,
+                dt(2022, 3, 31): 0.99,
+            },
+            interpolation="linear",
+            id="v",
+            convention="Act360",
+            ad=1,
+        )
+        expected = (
+            '{"nodes": {"2022-03-01": 1.0, "2022-03-31": 0.99}, '
+            '"interpolation": "linear", "t": null, "c": null, "id": "v", '
+            '"convention": "Act360", "endpoints": ["natural", "natural"], "modifier": "MF", '
+            '"calendar_type": "null", "ad": 1, "calendar": null}'
+        )
+        result = Serialise(curve).to_json()
+        assert result == expected
 
-    fxr3 = FXRates({"usdnok": 8.0, "eurusd": 1.05}, base="NOK")
-    assert fxr1 != fxr3  # base is different
+    def test_round_trip(self):
+        curve = Curve(
+            nodes={
+                dt(2022, 3, 1): 1.00,
+                dt(2022, 3, 31): 0.99,
+            },
+            interpolation="linear",
+            id="v",
+            convention="Act360",
+            ad=1,
+        )
+        data = Serialise(curve).to_json()
+        constructed = Serialise.from_json(Curve, data)
+        assert constructed == curve
+
+
+# def test_round_trip(curve, line_curve, index_curve):
+#     curve = Curve(
+#         nodes={
+#             dt(2022, 3, 1): 1.00,
+#             dt(2022, 3, 31): 0.99,
+#         },
+#         interpolation="linear",
+#         id="v",
+#         convention="Act360",
+#         ad=1,
+#     )
+#     serial = curve.to_json()
+#     constructed = Curve.from_json(serial)
+#     assert constructed == curve
+#
+#     serial = line_curve.to_json()
+#     constructed = LineCurve.from_json(serial)
+#     assert constructed == line_curve
+#
+#     serial = index_curve.to_json()
+#     constructed = IndexCurve.from_json(serial)
+#     assert constructed == index_curve
+#
+# def test_serialization_round_trip_spline():
+#     curve = Curve(
+#         nodes={
+#             dt(2022, 3, 1): 1.00,
+#             dt(2022, 3, 31): 0.99,
+#             dt(2022, 5, 1): 0.98,
+#             dt(2022, 6, 4): 0.97,
+#             dt(2022, 7, 4): 0.96,
+#         },
+#         interpolation="linear",
+#         id="v",
+#         convention="Act360",
+#         ad=1,
+#         t=[
+#             dt(2022, 5, 1),
+#             dt(2022, 5, 1),
+#             dt(2022, 5, 1),
+#             dt(2022, 5, 1),
+#             dt(2022, 6, 4),
+#             dt(2022, 7, 4),
+#             dt(2022, 7, 4),
+#             dt(2022, 7, 4),
+#             dt(2022, 7, 4),
+#         ],
+#     )
+#
+#     serial = curve.to_json()
+#     constructed = Curve.from_json(serial)
+#     assert constructed == curve
+#
+#
+# def test_serialization_curve_str_calendar():
+#     curve = Curve(
+#         nodes={
+#             dt(2022, 3, 1): 1.00,
+#             dt(2022, 3, 31): 0.99,
+#         },
+#         interpolation="linear",
+#         id="v",
+#         convention="Act360",
+#         modifier="F",
+#         calendar="LDN",
+#         ad=1,
+#     )
+#     serial = curve.to_json()
+#     constructed = Curve.from_json(serial)
+#     assert constructed == curve
+#
+#
+# def test_serialization_curve_custom_calendar():
+#     calendar = get_calendar("ldn")
+#     curve = Curve(
+#         nodes={
+#             dt(2022, 3, 1): 1.00,
+#             dt(2022, 3, 31): 0.99,
+#         },
+#         interpolation="linear",
+#         id="v",
+#         convention="Act360",
+#         modifier="F",
+#         calendar=calendar,
+#         ad=1,
+#     )
+#     serial = curve.to_json()
+#     constructed = Curve.from_json(serial)
+#     assert constructed == curve
