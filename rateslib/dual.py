6c7b3cccd71d8bf52a9638cc7f91b56b7c878051
JHM Darbyshire (MBP) <attack68@users.noreply.github.com>
2024-03-02 14:29:28 +0100

    REF: skip creating new object when setting order

--- rateslib/dual.py
+++ rateslib/dual.py
@@ -351,7 +351,7 @@ class Dual2(DualBase):
         if order == 1:
             return Dual(self.real, self.vars, self.dual)
         if order == 2:
-            return Dual2(self.real, self.vars, self.dual, self.dual2)
+            return self
         if order == 0:
             return float(self)
 
@@ -519,7 +519,7 @@ class Dual(DualBase):
 
     def _set_order(self, order):
         if order == 1:
-            return Dual(self.real, self.vars, self.dual)
+            return self
         if order == 2:
             return Dual2(self.real, self.vars, self.dual)
         if order == 0:
