dd968d0515dc7523f3cb2fe2eb447e633e8cc6e2
JHM Darbyshire (win) <24256554+attack68@users.noreply.github.com>
2024-03-18 16:24:54 +0100

    RUST: add pyo3 bindings

--- src/lib.rs
+++ src/lib.rs
@@ -12,20 +12,22 @@ use dual::dual2::Dual2;
 use dual::linalg_py::{dsolve1_py, dsolve2_py, fdsolve1_py, fdsolve2_py};
 
 pub mod splines;
-use splines::spline_py::{PPSplineF64, PPSplineDual, PPSplineDual2};
+use splines::spline_py::{PPSplineF64, PPSplineDual, PPSplineDual2, bsplev_single, bspldnev_single};
 
 #[pymodule]
 fn rateslibrs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Dual>()?;
     m.add_class::<Dual2>()?;
-    m.add_class::<PPSplineF64>()?;
-    m.add_class::<PPSplineDual>()?;
-    m.add_class::<PPSplineDual2>()?;
-
-    // m.add_class::<PPSpline<f64>>()?;
     m.add_function(wrap_pyfunction!(dsolve1_py, m)?)?;
     m.add_function(wrap_pyfunction!(dsolve2_py, m)?)?;
     m.add_function(wrap_pyfunction!(fdsolve1_py, m)?)?;
     m.add_function(wrap_pyfunction!(fdsolve2_py, m)?)?;
+
+    m.add_class::<PPSplineF64>()?;
+    m.add_class::<PPSplineDual>()?;
+    m.add_class::<PPSplineDual2>()?;
+    m.add_function(wrap_pyfunction!(bsplev_single, m)?)?;
+    m.add_function(wrap_pyfunction!(bspldnev_single, m)?)?;
+
     Ok(())
 }
