16042835bc990764ef9ff17fdfd2cd4f478bb610
JHM Darbyshire (win) <24256554+attack68@users.noreply.github.com>
2024-05-29 15:37:33 +0200

    ENH: add all v1.0 calendars to rust

--- src/calendars/named/mod.rs
+++ src/calendars/named/mod.rs
@@ -2,6 +2,10 @@ pub mod bus;
 pub mod nyc;
 pub mod tgt;
 pub mod ldn;
+pub mod stk;
+pub mod osl;
+pub mod zur;
+pub mod tro;
 
 use crate::calendars::calendar::HolCal;
 use std::collections::HashMap;
@@ -13,6 +17,10 @@ fn get_weekmask_by_name(name: &str) -> Vec<u8> {
         ("nyc", nyc::WEEKMASK),
         ("tgt", tgt::WEEKMASK),
         ("ldn", ldn::WEEKMASK),
+        ("stk", stk::WEEKMASK),
+        ("osl", osl::WEEKMASK),
+        ("zur", zur::WEEKMASK),
+        ("tro", zur::WEEKMASK),
     ]);
     hmap.get(name).unwrap().to_vec()
 }
@@ -23,6 +31,10 @@ fn get_holidays_by_name(name: &str) -> Vec<NaiveDateTime> {
         ("nyc", nyc::HOLIDAYS),
         ("tgt", tgt::HOLIDAYS),
         ("ldn", ldn::HOLIDAYS),
+        ("stk", stk::HOLIDAYS),
+        ("osl", osl::HOLIDAYS),
+        ("zur", zur::HOLIDAYS),
+        ("tro", tro::HOLIDAYS),
     ]);
     hmap.get(name).unwrap().iter().map(|x| NaiveDateTime::parse_from_str(x, "%Y-%m-%d %H:%M:%S").unwrap()).collect()
 }
@@ -71,6 +83,30 @@ mod tests {
     fn test_ldn() {
         let cal = get_calendar_by_name("ldn");
         assert!(cal.is_holiday(&NaiveDateTime::parse_from_str("2024-08-26 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()));
+    }
+
+    #[test]
+    fn test_stk() {
+        let cal = get_calendar_by_name("stk");
+        assert!(cal.is_holiday(&NaiveDateTime::parse_from_str("2024-06-06 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()));
+    }
+
+    #[test]
+    fn test_osl() {
+        let cal = get_calendar_by_name("osl");
+        assert!(cal.is_holiday(&NaiveDateTime::parse_from_str("2024-05-17 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()));
+    }
+
+    #[test]
+    fn test_zur() {
+        let cal = get_calendar_by_name("zur");
+        assert!(cal.is_holiday(&NaiveDateTime::parse_from_str("2024-08-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()));
+    }
+
+    #[test]
+    fn test_tro() {
+        let cal = get_calendar_by_name("tro");
+        assert!(cal.is_holiday(&NaiveDateTime::parse_from_str("2024-09-30 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()));
     }
 
 }
