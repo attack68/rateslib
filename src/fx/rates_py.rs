529c471bea98f9915d8ac294e6bd4aa4bf8e28b8
JHM Darbyshire <24256554+attack68@users.noreply.github.com>
2024-06-26 22:25:34 +0200

    remove unnecessary fields

--- src/fx/rates_py.rs
+++ src/fx/rates_py.rs
@@ -6,8 +6,8 @@ use chrono::prelude::*;
 use pyo3::prelude::*;
 // use std::collections::HashMap;
 use ndarray::Axis;
-use pyo3::exceptions::PyValueError;
-use pyo3::types::PyFloat;
+// use pyo3::exceptions::PyValueError;
+// use pyo3::types::PyFloat;
 
 #[pymethods]
 impl Ccy {
