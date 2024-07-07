539752c849475de51c623a2b4e7f054704934cc6
JHM Darbyshire <24256554+attack68@users.noreply.github.com>
2024-07-07 10:39:56 +0200

    REF: develop

--- src/dual/dual.rs
+++ src/dual/dual.rs
@@ -776,20 +776,7 @@ mod tests {
         }
     }
 
-    #[test]
-    fn negate() {
-        let d = Dual::try_new(
-            2.3,
-            Vec::from([String::from("a"), String::from("b")]),
-            Vec::from([2., -1.4]),
-        )
-        .unwrap();
-        let d2 = -d.clone();
-        assert!(d2.real == -2.3);
-        assert!(Arc::ptr_eq(&d.vars, &d2.vars));
-        assert!(d2.dual[0] == -2.0);
-        assert!(d2.dual[1] == 1.4);
-    }
+
 
     #[test]
     fn eq_ne() {
@@ -932,14 +919,7 @@ mod tests {
         assert_eq!(result, expected)
     }
 
-    #[test]
-    fn neg_ref() {
-        let d1 =
-            Dual::try_new(2.5, vec!["x".to_string(), "y".to_string()], vec![1.1, 2.2]).unwrap();
-        let d2 = -&d1;
-        assert_eq!(d2.real, -2.5);
-        assert_eq!(d2.dual, Array1::from_vec(vec![-1.1, -2.2]));
-    }
+
 
     #[test]
     fn pow_ref() {
@@ -1323,39 +1303,7 @@ mod tests {
         }
     }
 
-    #[test]
-    fn negate2() {
-        let d = Dual2::try_new(
-            2.3,
-            Vec::from([String::from("a"), String::from("b")]),
-            Vec::from([2., -1.4]),
-            Vec::from([1.0, -1.0, -1.0, 2.0]),
-        )
-        .unwrap();
-        let d2 = -d.clone();
-        assert!(d2.real == -2.3);
-        assert!(Arc::ptr_eq(&d.vars, &d2.vars));
-        assert!(d2.dual[0] == -2.0);
-        assert!(d2.dual[1] == 1.4);
-        assert!(d2.dual2[[1, 0]] == 1.0);
-    }
 
-    #[test]
-    fn negate_ref2() {
-        let d = Dual2::try_new(
-            2.3,
-            Vec::from([String::from("a"), String::from("b")]),
-            Vec::from([2., -1.4]),
-            Vec::from([1.0, -1.0, -1.0, 2.0]),
-        )
-        .unwrap();
-        let d2 = -&d;
-        assert!(d2.real == -2.3);
-        assert!(Arc::ptr_eq(&d.vars, &d2.vars));
-        assert!(d2.dual[0] == -2.0);
-        assert!(d2.dual[1] == 1.4);
-        assert!(d2.dual2[[1, 0]] == 1.0);
-    }
 
     #[test]
     fn one2() {
--- src/dual/dual_ops/neg.rs
+++ src/dual/dual_ops/neg.rs
@@ -34,3 +34,68 @@ impl_op!(-|a: &Dual2| -> Dual2 {
         dual2: &a.dual2 * -1.0,
     }
 });
+
+#[cfg(test)]
+mod tests {
+    use ndarray::Array1;
+    use super::*;
+
+    #[test]
+    fn negate() {
+        let d = Dual::try_new(
+            2.3,
+            Vec::from([String::from("a"), String::from("b")]),
+            Vec::from([2., -1.4]),
+        )
+            .unwrap();
+        let d2 = -d.clone();
+        assert!(d2.real == -2.3);
+        assert!(Arc::ptr_eq(&d.vars, &d2.vars));
+        assert!(d2.dual[0] == -2.0);
+        assert!(d2.dual[1] == 1.4);
+    }
+
+    #[test]
+    fn neg_ref() {
+        let d1 =
+            Dual::try_new(2.5, vec!["x".to_string(), "y".to_string()], vec![1.1, 2.2]).unwrap();
+        let d2 = -&d1;
+        assert_eq!(d2.real, -2.5);
+        assert_eq!(d2.dual, Array1::from_vec(vec![-1.1, -2.2]));
+    }
+
+    #[test]
+    fn negate2() {
+        let d = Dual2::try_new(
+            2.3,
+            Vec::from([String::from("a"), String::from("b")]),
+            Vec::from([2., -1.4]),
+            Vec::from([1.0, -1.0, -1.0, 2.0]),
+        )
+            .unwrap();
+        let d2 = -d.clone();
+        assert!(d2.real == -2.3);
+        assert!(Arc::ptr_eq(&d.vars, &d2.vars));
+        assert!(d2.dual[0] == -2.0);
+        assert!(d2.dual[1] == 1.4);
+        assert!(d2.dual2[[1, 0]] == 1.0);
+    }
+
+    #[test]
+    fn negate_ref2() {
+        let d = Dual2::try_new(
+            2.3,
+            Vec::from([String::from("a"), String::from("b")]),
+            Vec::from([2., -1.4]),
+            Vec::from([1.0, -1.0, -1.0, 2.0]),
+        )
+            .unwrap();
+        let d2 = -&d;
+        assert!(d2.real == -2.3);
+        assert!(Arc::ptr_eq(&d.vars, &d2.vars));
+        assert!(d2.dual[0] == -2.0);
+        assert!(d2.dual[1] == 1.4);
+        assert!(d2.dual2[[1, 0]] == 1.0);
+    }
+
+}
