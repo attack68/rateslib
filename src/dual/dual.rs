1a65798ec56b769c87df08ec905260b343f69c5a
JHM Darbyshire <24256554+attack68@users.noreply.github.com>
2024-07-07 10:37:48 +0200

    REF: develop

--- src/dual/dual.rs
+++ src/dual/dual.rs
@@ -962,30 +962,7 @@ mod tests {
         assert_eq!(d2.abs_sub(&d1), Dual::zero());
     }
 
-    #[test]
-    fn rem_() {
-        let d1 = Dual::try_new(10.0, vec!["x".to_string()], vec![2.0]).unwrap();
-        let d2 = Dual::new(3.0, vec!["x".to_string()]);
-        let result = d1 % d2;
-        let expected = Dual::try_new(1.0, vec!["x".to_string()], vec![-1.0]).unwrap();
-        assert_eq!(result, expected);
-    }
 
-    #[test]
-    fn rem_f64_() {
-        let d1 = Dual::try_new(10.0, vec!["x".to_string()], vec![2.0]).unwrap();
-        let result = &d1 % 3.0_f64;
-        assert_eq!(
-            result,
-            Dual::try_new(1.0, vec!["x".to_string()], vec![2.0]).unwrap()
-        );
-
-        let result = 11.0_f64 % d1;
-        assert_eq!(
-            result,
-            Dual::try_new(1.0, vec!["x".to_string()], vec![-2.0]).unwrap()
-        );
-    }
 
     #[test]
     fn is_zero_() {
@@ -1407,30 +1384,7 @@ mod tests {
         assert_eq!(d2.abs_sub(&d1), Dual2::zero());
     }
 
-    #[test]
-    fn rem_2() {
-        let d1 = Dual2::try_new(10.0, vec!["x".to_string()], vec![2.0], vec![]).unwrap();
-        let d2 = Dual2::new(3.0, vec!["x".to_string()]);
-        let result = d1 % d2;
-        let expected = Dual2::try_new(1.0, vec!["x".to_string()], vec![-1.0], vec![]).unwrap();
-        assert_eq!(result, expected);
-    }
 
-    #[test]
-    fn rem_f64_2() {
-        let d1 = Dual2::try_new(10.0, vec!["x".to_string()], vec![2.0], vec![]).unwrap();
-        let result = &d1 % 3.0_f64;
-        assert_eq!(
-            result,
-            Dual2::try_new(1.0, vec!["x".to_string()], vec![2.0], vec![]).unwrap()
-        );
-
-        let result = 11.0_f64 % d1;
-        assert_eq!(
-            result,
-            Dual2::try_new(1.0, vec!["x".to_string()], vec![-2.0], vec![]).unwrap()
-        );
-    }
 
     #[test]
     fn is_zero2() {
--- src/dual/dual_ops/rem.rs
+++ src/dual/dual_ops/rem.rs
@@ -22,3 +22,58 @@ impl_op_ex!(% |a: &Dual2, b: &Dual2| -> Dual2 {
     let d = f64::trunc(a.real / b.real);
     a - d * b
 });
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rem_() {
+        let d1 = Dual::try_new(10.0, vec!["x".to_string()], vec![2.0]).unwrap();
+        let d2 = Dual::new(3.0, vec!["x".to_string()]);
+        let result = d1 % d2;
+        let expected = Dual::try_new(1.0, vec!["x".to_string()], vec![-1.0]).unwrap();
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn rem_f64_() {
+        let d1 = Dual::try_new(10.0, vec!["x".to_string()], vec![2.0]).unwrap();
+        let result = &d1 % 3.0_f64;
+        assert_eq!(
+            result,
+            Dual::try_new(1.0, vec!["x".to_string()], vec![2.0]).unwrap()
+        );
+
+        let result = 11.0_f64 % d1;
+        assert_eq!(
+            result,
+            Dual::try_new(1.0, vec!["x".to_string()], vec![-2.0]).unwrap()
+        );
+    }
+
+    #[test]
+    fn rem_2() {
+        let d1 = Dual2::try_new(10.0, vec!["x".to_string()], vec![2.0], vec![]).unwrap();
+        let d2 = Dual2::new(3.0, vec!["x".to_string()]);
+        let result = d1 % d2;
+        let expected = Dual2::try_new(1.0, vec!["x".to_string()], vec![-1.0], vec![]).unwrap();
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn rem_f64_2() {
+        let d1 = Dual2::try_new(10.0, vec!["x".to_string()], vec![2.0], vec![]).unwrap();
+        let result = &d1 % 3.0_f64;
+        assert_eq!(
+            result,
+            Dual2::try_new(1.0, vec!["x".to_string()], vec![2.0], vec![]).unwrap()
+        );
+
+        let result = 11.0_f64 % d1;
+        assert_eq!(
+            result,
+            Dual2::try_new(1.0, vec!["x".to_string()], vec![-2.0], vec![]).unwrap()
+        );
+    }
+}
