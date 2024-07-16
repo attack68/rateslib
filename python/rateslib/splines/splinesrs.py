0952afc134a6260bedf64b817584f6fbef29b642
JHM Darbyshire (MacBookAir) <attack68@users.noreply.github.com>
2024-07-16 20:46:13 +0200

    REF: REMOVE PYTHON SPLINES CODE

--- python/rateslib/splines/splinesrs.py
+++
@@ -1,5 +0,0 @@
-from rateslib.rs import PPSplineDual, PPSplineDual2, PPSplineF64, bspldnev_single, bsplev_single
-
-PPSplineF64.__doc__ = "Piecewise polynomial spline composed of float values on the x and y axes."
-PPSplineDual.__doc__ = "Piecewise polynomial spline composed of float values on the x-axis and Dual values on the y-axis."
-PPSplineDual2.__doc__ = "Piecewise polynomial spline composed of float values on the x-axis and Dual2 values on the y-axis."
