8a23d6ee8c224e297f83c78a32b403e2586d8700
JHM Darbyshire (win) <24256554+attack68@users.noreply.github.com>
2024-07-31 10:48:49 +0200

    FieldOps

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,45 @@
-// use std::time::{Duration, SystemTime};
-use chrono::prelude::*;
-use chrono::Days;
+
+use std::time::SystemTime;
+use rateslib::dual::dual::{Dual, DualsOrF64, FieldOps};
+
+fn ops<T>(a: &T, b: &T) -> T
+where         T: FieldOps<T>,
+  for<'a> &'a T: FieldOps<T>
+{
+    &(&(&(a + b) - a) * b) / a
+}
 
 fn main() {
-    let list: Vec<f64> = (0..100).map(f64::from).collect();
+    let a0 = 2.5_f64;
+    let b0 = 3.5_f64;
+    let a1 = Dual::new(2.5_f64, vec!["x".to_string(), "y".to_string()]);
+    let b1 = Dual::new_from(&a1, 3.5_f64, vec!["x".to_string(), "y".to_string()]);
+    let a2 = DualsOrF64::Dual(a1.clone());
+    let b2 = DualsOrF64::Dual(b1.clone());
+    let a3 = DualsOrF64::F64(2.5_f64);
+    let b3 = DualsOrF64::F64(3.5_f64);
+
     let now = SystemTime::now();
-    let mut result = 0;
-    for i in 0..1000000 {
-        result = index_left(&list[..], &25_f64, None);
+
+    for i in 0..1000 {
+        let _ = ops(&a0, &b0);
     }
-    println!("{:.5?} seconds", now.elapsed());
-    println!("{}", result);
+    println!("{:.5?} time taken for f64", now.elapsed());
+
+    for i in 0..1000 {
+        let _ = ops(&a3, &b3);
+    }
+    println!("{:.5?} time taken for DualsOrF64 F64 wrapper", now.elapsed());
+
+    for i in 0..1000 {
+        let _ = ops(&a1, &b1);
+    }
+    println!("{:.5?} time taken for Dual", now.elapsed());
+
+    for i in 0..1000 {
+        let _ = ops(&a2, &b2);
+    }
+    println!("{:.5?} time taken for DualsOrF64 Dual wrapper", now.elapsed());
+
+
 }
