BUG: correct gradient calculation for dependency chains with mismatching numbers of vars/instruments

python/rateslib/solver.py
@@ -490,12 +490,12 @@ class Gradients:
                 grad_v_r = np.array([gradient(r, pre_solver.pre_variables) for r in self.r]).T
                 block = np.matmul(grad_v_r, self.grad_s_vT)
                 block = -1 * np.matmul(pre_solver.grad_s_vT_pre, block)
-                grad_s_vT[i : i + m, -self.m :] = block
+                grad_s_vT[i : i + m, -self.n :] = block
 
                 i, j = i + m, j + n
 
             # create bottom right block
-            grad_s_vT[-self.m :, -self.m :] = self.grad_s_vT
+            grad_s_vT[-self.m :, -self.n :] = self.grad_s_vT
             self._grad_s_vT_pre = grad_s_vT
         return self._grad_s_vT_pre
 
@@ -1551,7 +1551,7 @@ class Solver(Gradients, _WithState):
                 # results in the same solution and this is erroneously categorised
                 # as a converged solution.
                 return self._solver_result(1, i, time() - t0)
-            elif self.g.real < self.func_tol:
+            elif i != 0 and self.g.real < self.func_tol:
                 return self._solver_result(2, i, time() - t0)
 
             # v_0 = self.v.copy()