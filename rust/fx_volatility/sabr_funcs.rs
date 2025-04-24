badf609dd4d79e824db39eefbebd79361ae0743f
JHM Darbyshire (win11) <attack68@users.noreply.github.com>
2025-04-24 16:46:46 +0200

    REF: allow beta as Number in X0

--- rust/fx_volatility/sabr_funcs.rs
+++ rust/fx_volatility/sabr_funcs.rs
@@ -11,7 +11,7 @@ pub(crate) fn _sabr_x0(
     f: Number,
     _t: Number,
     a: Number,
-    b: f64,
+    b: Number,
     _p: Number,
     _v: Number,
     derivative: u8,
@@ -20,16 +20,16 @@ pub(crate) fn _sabr_x0(
     //If ``derivative`` is 1 also returns dX0/dk, calculated using sympy.
     //If ``derivative`` is 2 also returns dX0/df, calculated using sympy.
     let x0 = 1_f64 / &k;
-    let x1 = 1_f64 / 24_f64 - b / 24_f64;
+    let x1 = 1_f64 / 24_f64 - &b / 24_f64;
     let x2 = (&f * &x0).log();
-    let x3 = (1_f64 - b).pow(4_f64);
-    let x4 = x1 * (&x2).pow(2_f64) + (&x2).pow(4_f64) * x3 / 1920_f64 + 1_f64;
-    let x5 = b / 2_f64 - 0.5_f64;
-    let x6 = &a * (&f * &k).pow(x5);
+    let x3 = (1_f64 - &b).pow(4_f64);
+    let x4 = &x1 * (&x2).pow(2_f64) + (&x2).pow(4_f64) * &x3 / 1920_f64 + 1_f64;
+    let x5 = &b / 2_f64 - 0.5_f64;
+    let x6 = &a * (&f * &k).pow(&x5);
 
     let x = &x6
-        / ((&x2).pow(4_f64) * (1_f64 - b).pow(4_f64) / 1920_f64
-            + (&x2).pow(2_f64) * (1_f64 / 24_f64 - b / 24_f64)
+        / ((&x2).pow(4_f64) * (1_f64 - &b).pow(4_f64) / 1920_f64
+            + (&x2).pow(2_f64) * (1_f64 / 24_f64 - &b / 24_f64)
             + 1_f64);
 
     let dx: Option<Number> = match derivative {
@@ -39,11 +39,11 @@ pub(crate) fn _sabr_x0(
                     / x4.pow(2_f64),
         ),
         2 => {
-            let y0 = b - 1_f64;
+            let y0 = &b - 1_f64;
             let y2 = (&y0).pow(2_f64) * (&x2).pow(2_f64);
             let y3 = (&y0).pow(4_f64) * (&x2).pow(4_f64) + 80_f64 * &y2 + 1920_f64;
             Some(
-                960_f64 * &a * y0 * (&f * &k).pow(x5) * (-8_f64 * y0 * x2 * (&y2 + 40_f64) + &y3)
+                960_f64 * &a * &y0 * (&f * &k).pow(x5) * (-8_f64 * y0 * x2 * (&y2 + 40_f64) + &y3)
                     / (f * y3.pow(2_f64)),
             )
         }
