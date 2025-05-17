8bc62195d1fb9fdd13ec5c834f9cfc6939fa6bae
JHM Darbyshire <24256554+attack68@users.noreply.github.com>
2025-05-17 10:30:50 +0200

    REF: series of amends to Curve interpolation

--- python/rateslib/curves/curves.py
+++ python/rateslib/curves/curves.py
@@ -5,7 +5,7 @@ import warnings
 from calendar import monthrange
 from collections.abc import Callable
 from datetime import datetime, timedelta
-from math import comb, floor
+from math import comb
 from typing import TYPE_CHECKING, Any, TypeAlias
 from uuid import uuid4
 
@@ -3250,100 +3250,6 @@ def interpolate(
         )
     ret = op(y_1 + (y_2 - y_1) * ((x - x_1) / (x_2 - x_1)))
     return ret
-
-
-def index_left(
-    list_input: list[Any],
-    list_length: int,
-    value: Any,
-    left_count: int = 0,
-) -> int:
-    """
-    Return the interval index of a value from an ordered input list on the left side.
-
-    Parameters
-    ----------
-    input : list
-        Ordered list (lowest to highest) containing datatypes the same as value.
-    length : int
-        The length of ``input``.
-    value : Any
-        The value for which to determine the list index of.
-    left_count : int
-        The counter to pass recursively to determine the output. Users should not
-        directly specify, it is used in internal calculation only.
-
-    Returns
-    -------
-    int : The left index of the interval within which value is found (or extrapolated
-          from)
-
-    Notes
-    -----
-    Uses a binary search method which operates with time :math:`O(log_2 n)`.
-
-    Examples
-    --------
-    .. ipython:: python
-
-       from rateslib.curves import index_left
-
-    Out of domain values return the left-side index of the closest matching interval.
-    100 is attributed to the interval (1, 2].
-
-    .. ipython:: python
-
-       list = [0, 1, 2]
-       index_left(list, 3, 100)
-
-    -100 is attributed to the interval (0, 1].
-
-    .. ipython:: python
-
-       index_left(list, 3, -100)
-
-    Interior values return the left-side index of the interval.
-    1.45 is attributed to the interval (1, 2].
-
-    .. ipython:: python
-
-       index_left(list, 3, 1.45)
-
-    1 is attributed to the interval (0, 1].
-
-    .. ipython:: python
-
-       index_left(list, 3, 1)
-
-    """
-    if list_length == 1:
-        raise ValueError("`index_left` designed for intervals. Cannot index list of length 1.")
-
-    if list_length == 2:
-        return left_count
-
-    split: int = floor((list_length - 1) / 2)
-    if list_length == 3 and value == list_input[split]:
-        return left_count
-
-    if value <= list_input[split]:
-        return index_left(list_input[: split + 1], split + 1, value, left_count)
-    else:
-        return index_left(list_input[split:], list_length - split, value, left_count + split)
-
-
-# # ALTERNATIVE index_left: exhaustive search which is inferior to binary search
-# def index_left_exhaustive(list_input, value, left_count=0):
-#     if left_count == 0:
-#         if value > list_input[-1]:
-#             return len(list_input)-2
-#         if value <= list_input[0]:
-#             return 0
-#
-#     if list_input[0] < value <= list_input[1]:
-#         return left_count
-#     else:
-#         return index_left_exhaustive(list_input[1:], value, left_count + 1)
 
 
 def index_value(
