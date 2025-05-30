f34ed0b5cb60cf721d3da72ca45cf78f2dd4ae9c
JHM Darbyshire <24256554+attack68@users.noreply.github.com>
2025-05-30 20:29:14 +0200

    DOC: add info

--- python/tests/test_default.py
+++ python/tests/test_default.py
@@ -22,12 +22,15 @@ def test_context_raises() -> None:
 
 def test_reset_defaults() -> None:
     defaults.modifier = "MP"
+    defaults.base_currency = "gbp"
     assert defaults.modifier == "MP"
+    assert defaults.base_currency == "gbp"
     defaults.calendars["TEST"] = 10.0
     assert defaults.calendars["TEST"] == 10.0
 
     defaults.reset_defaults()
     assert defaults.modifier == "MF"
+    assert defaults.base_currency == "usd"
     assert "TEST" not in defaults.calendars
 
 
