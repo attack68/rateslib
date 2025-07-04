bf2b5ea32bfd28fb840125b149f907c84fa6ea5a
mikelync <211784556+mikelync@users.noreply.github.com>
2025-07-04 14:45:37 +0200

    ENH: Rust Schedule Development (#16)
    Co-authored-by: Mike Lync <mikelync@users.noreply.github.com>
    (cherry picked from commit d321de2d8eeffc9941648edcd2d752512c71ec6d)

--- python/tests/test_rust.py
+++ python/tests/test_rust.py
@@ -12,17 +12,17 @@ def test_stub_inference_equality():
 @pytest.mark.parametrize(
     ("method", "args", "exp"),
     [
-        ("next", (dt(2000, 1, 1),), dt(2000, 1, 11)),
-        ("previous", (dt(2000, 1, 11),), dt(2000, 1, 1)),
+        ("unext", (dt(2000, 1, 1),), dt(2000, 1, 11)),
+        ("uprevious", (dt(2000, 1, 11),), dt(2000, 1, 1)),
         (
             "uregular",
             (dt(2000, 1, 1), dt(2000, 1, 21)),
             [dt(2000, 1, 1), dt(2000, 1, 11), dt(2000, 1, 21)],
         ),
-        ("infer_front_stub", (dt(2000, 1, 1), dt(2000, 1, 17), True), dt(2000, 1, 7)),
-        ("infer_front_stub", (dt(2000, 1, 1), dt(2000, 1, 27), False), dt(2000, 1, 17)),
-        ("infer_back_stub", (dt(2000, 1, 1), dt(2000, 1, 17), True), dt(2000, 1, 11)),
-        ("infer_back_stub", (dt(2000, 1, 1), dt(2000, 1, 27), False), dt(2000, 1, 11)),
+        ("infer_ufront_stub", (dt(2000, 1, 1), dt(2000, 1, 17), True), dt(2000, 1, 7)),
+        ("infer_ufront_stub", (dt(2000, 1, 1), dt(2000, 1, 27), False), dt(2000, 1, 17)),
+        ("infer_uback_stub", (dt(2000, 1, 1), dt(2000, 1, 17), True), dt(2000, 1, 11)),
+        ("infer_uback_stub", (dt(2000, 1, 1), dt(2000, 1, 27), False), dt(2000, 1, 11)),
     ],
 )
 def test_frequency(method, args, exp):
@@ -34,7 +34,7 @@ def test_frequency(method, args, exp):
 def test_frequency_busday():
     cal = Cal([], [5, 6])
     f = Frequency.BusDays(5, cal)
-    result = f.next(dt(2023, 1, 2))
+    result = f.unext(dt(2023, 1, 2))
     assert result == dt(2023, 1, 9)
 
 
