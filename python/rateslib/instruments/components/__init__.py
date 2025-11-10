243e8aac32fc94a3c6c5ed260b79342cfe5d0a3d
mikelync <211784556+mikelync@users.noreply.github.com>
2025-11-10 12:33:52 +0100

    ENH: add  `ZCIS`  (#154)
    Co-authored-by: Mike Lync <mikelync@users.noreply.github.com>
    (cherry picked from commit c742c17e1d75358352bcbd4596c570153a063d0f)

--- python/rateslib/instruments/components/__init__.py
+++ python/rateslib/instruments/components/__init__.py
@@ -4,15 +4,17 @@ from rateslib.instruments.components.fx_exchange import FXExchange
 from rateslib.instruments.components.fx_vol_value import FXVolValue
 from rateslib.instruments.components.iirs import IIRS
 from rateslib.instruments.components.irs import IRS
-from rateslib.instruments.components.zcs import ZCS
 from rateslib.instruments.components.portfolio import Portfolio
 from rateslib.instruments.components.sbs import SBS
 from rateslib.instruments.components.spread import Spread
 from rateslib.instruments.components.value import Value
+from rateslib.instruments.components.zcis import ZCIS
+from rateslib.instruments.components.zcs import ZCS
 
 __all__ = [
     "IRS",
     "ZCS",
+    "ZCIS",
     "IIRS",
     "CDS",
     "SBS",
