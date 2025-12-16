c44d39af539a0822f826ea2ca197fbdb61b5582c
mikelync <211784556+mikelync@users.noreply.github.com>
2025-12-16 14:01:10 +0100

    DOC: reshuffle amortization documentation to `Amortization`  (#203)
    Co-authored-by: JHM Darbyshire (M1) <attack68@users.noreply.github.com>
    Co-authored-by: Mike Lync <mikelync@users.noreply.github.com>
    
    (cherry picked from commit 0e7cfdf01126ca9f008c3861685a790179b220c3)

--- python/rateslib/legs/amortization.py
+++ python/rateslib/legs/amortization.py
@@ -57,6 +57,197 @@ class Amortization:
       - *"to_zero"*: each period will be a constant value ending with zero implied ending balance.
       - *"{float}%"*: each period will amortize by a constant percentage of the outstanding balance.
 
+
+    .. rubric:: Using Amortization with Instruments
+
+    This section exemplifies how to use :class:`~rateslib.legs.Amortization` with instruments.
+
+      **Key Points**
+
+      - Amortization can be added to *Instruments* using the per leg ``amortization`` argument.
+      - It supports constant notional amortization, or custom schedules or
+        the :class:`~rateslib.legs.Amortization` class can be used to calculate other simple
+        structures.
+      - Some *Instruments* have not yet integrated amortization into their calculation, such as
+        *Bonds*.
+
+    **Standard Amortization**
+
+    The :class:`~rateslib.legs.FixedLeg` and :class:`~rateslib.legs.FloatLeg` classes both
+    have ``amortization`` as an input argument. An :class:`~rateslib.legs.Amortization` class
+    can be directly supplied or other values are internally passed to this class for
+    syntactic convenience.
+
+    The simplest, and most common, type of ``amortization`` to apply is a constant notional
+    per period.
+
+    .. ipython:: python
+       :suppress:
+
+       from rateslib import XCS, IRS, IndexFixedRateBond, FixedRateBond
+       from rateslib.legs import FixedLeg, FloatLeg, Amortization
+       from rateslib.scheduling import Schedule
+       from datetime import datetime as dt
+
+    .. tabs::
+
+       .. tab:: FixedLeg
+
+          .. ipython:: python
+
+             fxl = FixedLeg(
+                 schedule=Schedule(dt(2000, 1, 1), "1y", "Q"),
+                 notional=10e6,
+                 amortization=1e6,      # <- 1mm reduction per period
+             )
+             fxl.cashflows()[["Type", "Acc Start", "Notional"]]
+
+       .. tab:: FloatLeg
+
+          .. ipython:: python
+
+             fll = FloatLeg(
+                 schedule=Schedule(dt(2000, 1, 1), "1y", "M"),
+                 notional=10e6,
+                 amortization=0.5e6,    # 0.5mm reduction per period
+             )
+             fll.cashflows()[["Type", "Acc Start", "Notional"]]
+
+    Here, the *amortization* is expressed in a specific notional amount reduction per period so,
+    when applied to an :class:`~rateslib.instruments.IRS`, each leg with different
+    frequencies should be input directly.
+
+    If a *Leg* has a *final notional exchange* then any amortized amount would, under standard
+    convention, be paid out at the same time as the notional change. The final cashflow will be
+    reduced by the amount of interim exchanges that have already occurred. This can be
+    exemplified on a :class:`~rateslib.instruments.XCS`.
+
+    .. tabs::
+
+       .. tab:: IRS
+
+          .. ipython:: python
+
+             irs = IRS(
+                 effective=dt(2000, 1, 1),
+                 termination="1Y",
+                 frequency="Q",
+                 leg2_frequency="S",
+                 notional=1e6,
+                 amortization=2e5,       # <- Reduces notional on 1st July to 600,000
+                 leg2_amortization=-4e5, # <- Aligns the notional on 1st July
+             )
+             irs.cashflows()[["Type", "Acc Start", "Notional"]]
+
+       .. tab:: Non-MTM XCS
+
+          .. ipython:: python
+
+             xcs = XCS(
+                 effective=dt(2000, 1, 1),
+                 termination="1y",
+                 spec="eurusd_xcs",
+                 notional=5e6,
+                 amortization=1e6,      # <- 1mm reduction and notional exchange per period
+                 leg2_mtm=False,
+             )
+             xcs.cashflows()[["Type", "Period", "Acc Start", "Payment", "Ccy", "Notional", "Reference Ccy"]]
+
+       .. tab:: MTM XCS
+
+          Mark-to-market :class:`~rateslib.instruments.XCS` also support ``amortization`` which
+          affects the MTM cashflows respectively.
+
+          .. ipython:: python
+
+             xcs = XCS(
+                 effective=dt(2000, 1, 1),
+                 termination="1y",
+                 spec="eurusd_xcs",
+                 notional=5e6,
+                 amortization=1e6,      # <- 1mm reduction and notional exchange per period
+                 leg2_mtm=True,
+             )
+             xcs.cashflows()[["Type", "Period", "Acc Start", "Payment", "Ccy", "Notional", "Reference Ccy"]]
+
+    .. rubric:: Custom Amortization
+
+    By using the :class:`~rateslib.legs.Amortization` class custom amortization can be directly
+    input to an *Instrument*. The following examples are the same, with the first being
+    syntactic convenience for the second. The above examples are also syntactic convenience for
+    applying the same amortization amount each period.
+
+    .. tabs::
+
+       .. tab:: Amortization List
+
+          .. ipython:: python
+
+             irs = IRS(
+                 effective=dt(2000, 1, 1),
+                 termination="1Y",
+                 frequency="Q",
+                 leg2_frequency="S",
+                 notional=1e6,
+                 amortization=[100000, 300000, -5000],    # <- Reduces notional on 1st July to 600,000
+                 leg2_amortization=[-400000], # <- Aligns the notional on 1st July
+             )
+             irs.cashflows()[["Type", "Acc Start", "Notional"]]
+
+       .. tab:: Amortization Object
+
+          .. ipython:: python
+
+             irs = IRS(
+                 effective=dt(2000, 1, 1),
+                 termination="1Y",
+                 frequency="Q",
+                 leg2_frequency="S",
+                 notional=1e6,
+                 amortization=Amortization(4, 1e6, [100000, 300000, -5000]),
+                 leg2_amortization=Amortization(2, -1e6, [-400000])
+             )
+             irs.cashflows()[["Type", "Acc Start", "Notional"]]
+
+    .. rubric:: Unsupported Instruments
+
+    *Instruments* that currently do **not** support amortization are *Bonds*.
+
+    .. tabs::
+
+       .. tab:: FixedRateBond
+
+          .. ipython:: python
+
+             try:
+                 FixedRateBond(
+                     effective=dt(2000, 1, 1),
+                     termination="1y",
+                     spec="us_gb",
+                     notional=5e6,
+                     amortization=1e6,
+                     fixed_rate=2.0,
+                 )
+             except Exception as e:
+                 print(e)
+
+       .. tab:: IndexFixedRateBond
+
+          .. ipython:: python
+
+             try:
+                 IndexFixedRateBond(
+                     effective=dt(2000, 1, 1),
+                     termination="1y",
+                     spec="us_gb",
+                     notional=5e6,
+                     amortization=1e6,
+                     fixed_rate=2.0,
+                     index_base=100.0,
+                 )
+             except Exception as e:
+                 print(e)
+
     """
 
     _type: _AmortizationType
