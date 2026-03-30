f903c83dbd6795257500b1c2084c2979375fbd89
JHM Darbyshire (win11) <attack68@users.noreply.github.com>
2026-03-30 17:48:58 +0200

    RLS: 2.7.x

--- python/rateslib/_spec_loader.py
+++ python/rateslib/_spec_loader.py
@@ -22,7 +22,7 @@ if TYPE_CHECKING:
         Any,
     )
 
-DEVELOPMENT = os.environ.get("RATESLIB_DEVELOPMENT", "True")
+DEVELOPMENT = os.environ.get("RATESLIB_DEVELOPMENT", "False")
 
 # This is output from a development version and hard coded before a release for performance.
 INSTRUMENT_SPECS: dict[str, dict[str, Any]] = {
